5558fbfc8a9c10abdf8bee7c861a65366b088c79
Andy Royle <ajroyle@gmail.com>
2016-02-15 15:16:05 +0000

    add a StatsDTiming attribute for hooking in AOP

---
+++ Ve.Metrics.StatsDClient/StatsDTiming.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ve.Metrics.StatsDClient
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class StatsDTiming : Attribute
+    {
+        public string Name { get; set; }
+        public Dictionary<string,string> Tags { get; set; } 
+
+        public StatsDTiming(string name)
+        {
+            Name = name;
+        }
+    }
+}
