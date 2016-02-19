2892ae7fbe676e17146dde005e9eb8118af5627c
Andy Royle <ajroyle@gmail.com>
2016-02-19 12:04:21 +0000

    add tests for unity interceptors

---
+++ Ve.Metrics.StatsDClient.Tests/Unity/UnityInterceptorsTestBase.cs
@@ -0,0 +1,32 @@
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.InterceptionExtension;
+using Moq;
+using NUnit.Framework;
+using Ve.Metrics.StatsDClient.Attributes;
+using Ve.Metrics.StatsDClient.Unity;
+
+namespace Ve.Metrics.StatsDClient.Tests.Unity
+{
+    public class UnityInterceptorsTestBase<T1 ,T2> where T1: BaseInterceptor<T2>, IInterceptionBehavior where T2: StatsDBase
+    {
+        protected IUnityContainer Container;
+        protected Mock<IVeStatsDClient> StatsdMock;
+        protected IFooService Service;
+
+        [SetUp]
+        protected void Setup()
+        {
+            StatsdMock = new Mock<IVeStatsDClient>();
+            Container = new UnityContainer();
+
+            Container.RegisterInstance(StatsdMock.Object);
+            Container.AddNewExtension<Interception>();
+            Container.RegisterType<IFooService, FooService>(
+                new Interceptor<InterfaceInterceptor>(),
+                new InterceptionBehavior<T1>()
+                );
+
+            Service = Container.Resolve<IFooService>();
+        }
+    }
+}
