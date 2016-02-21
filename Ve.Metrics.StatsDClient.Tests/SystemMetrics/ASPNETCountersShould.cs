ea6f5113f3635d74aeed96abc9076f6d39273049
Gabriel Nepomuceno <gnepomuceno@veinteractive.com>
2016-02-21 08:47:54 +0000

    Including asp.net performance counters
    One easy way for those using asp.net to include all the asp.net performance counters
    
    There are more system counters that could be gathered but this are up do discussion
    The idea would be that this could be a blueprint to a more generic performance counter gathered that could be used passing only the category
    Or even one that gets all the performance counters from the machine.

---
+++ Ve.Metrics.StatsDClient.Tests/SystemMetrics/ASPNETCountersShould.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+using Ve.Metrics.StatsDClient.SystemMetrics;
+
+namespace Ve.Metrics.StatsDClient.Tests.SystemMetrics
+{
+    [TestFixture]
+    public class AspNetCountersShould
+    {
+        private Mock<IVeStatsDClient> _statsd;
+        private AspNetCounters _metric;
+
+        [SetUp]
+        public void Setup()
+        {
+            _statsd = new Mock<IVeStatsDClient>();
+            _statsd.Setup(x => x.LogGauge(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<Dictionary<string, string>>()));
+            _statsd.Setup(x => x.LogCount(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<Dictionary<string, string>>()));
+            _metric = new AspNetCounters();
+
+            _metric.Execute(_statsd.Object);
+        }
+
+        [Test]
+        public void It_should_implement_the_name_property()
+        {
+            _metric.Name.ShouldBe("AspNetCounters");
+        }
+
+        [Test]
+        public void It_should_get_the_aspnet_counters_and_log_it_to_statsd()
+        {
+            _statsd.Verify(x => x.LogGauge(It.Is<string>((s) => s.StartsWith("aspnet")),It.IsAny<int>(), It.IsAny<Dictionary<string,string>>()));
+            _statsd.Verify(x => x.LogCount(It.Is<string>((s) => s.StartsWith("aspnet")), It.IsAny<int>(), It.IsAny<Dictionary<string, string>>()));
+
+        }
+    }
+}
