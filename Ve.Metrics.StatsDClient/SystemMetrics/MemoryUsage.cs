27a32e8667803dfa9b409a285a153689181cb0ce
Andy Royle <ajroyle@gmail.com>
2016-02-24 15:28:16 +0000

    remove performance counter logic

--- Ve.Metrics.StatsDClient/SystemMetrics/MemoryUsage.cs
+++ Ve.Metrics.StatsDClient/SystemMetrics/MemoryUsage.cs
@@ -4,21 +4,12 @@ namespace Ve.Metrics.StatsDClient.SystemMetrics
 {
     public class MemoryUsage : SystemMetric
     {
-        private readonly PerformanceCounter _availableMemoryCounter;
-
         public override string Name => "MemoryUsage";
-
-        public MemoryUsage()
-        {
-            _availableMemoryCounter = new PerformanceCounter("Memory", "Available MBytes");
-        }
 
         public override void Execute(IVeStatsDClient client)
         {
             var memoryUsageInMegaBytes = (System.Diagnostics.Process.GetCurrentProcess().WorkingSet64 / 1024) / 1024;
-            
             client.LogGauge("process.memoryusage", (int) memoryUsageInMegaBytes);
-            client.LogGauge("system.memory.free", (int) _availableMemoryCounter.NextValue());
         }
     }
 }
